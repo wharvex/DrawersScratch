9e1b1dee3690098dfe0fe88c52e99b59c5f79e57
Tim <81175852+wharvex@users.noreply.github.com>
2024-09-04 16:33:01 -0400

    database works now

---
+++ DrawersScratch/DrawersScratch/Utils/SqlUtils.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace DrawersScratch.Utils;
+
+public class SqlUtils
+{
+    // https://stackoverflow.com/a/67144114/16458003
+    public static bool TableAlreadyExists(SqliteConnection openConnection, string tableName)
+    {
+        var sql = "SELECT name FROM sqlite_master WHERE type='table' AND name='" + tableName + "';";
+        if (openConnection.State == System.Data.ConnectionState.Open)
+        {
+            SqliteCommand command = new SqliteCommand(sql, openConnection);
+            SqliteDataReader reader = command.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Close();
+                return true;
+            }
+            reader.Close();
+            return false;
+        }
+        else
+        {
+            throw new System.ArgumentException("Data.ConnectionState must be open");
+        }
+    }
+}
--- DrawersScratch/DrawersScratch/ViewModels/MainViewModel.cs
+++ DrawersScratch/DrawersScratch/ViewModels/MainViewModel.cs
@@ -1,104 +1,133 @@
 ﻿using System;
-using System.IO;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using DrawersScratch.Utils;
 using Microsoft.Data.Sqlite;
 
-namespace DrawersScratch.ViewModels
+namespace DrawersScratch.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
 {
-    public partial class MainViewModel : ViewModelBase
+    private const string DbPath = "hello.db";
+    private const string DbTableName = "user";
+    private long _dbLastRowId;
+
+    [ObservableProperty]
+    private string _headingColumn0 = "Add To Database";
+
+    [ObservableProperty]
+    private string _headingColumn1 = "Read From Database";
+
+    [ObservableProperty]
+    private string _headingColumn2 = "Log";
+
+    [ObservableProperty]
+    private string _textColumn1 = "Database Contents:";
+
+    [ObservableProperty]
+    private string _textColumn2 = "Log:";
+
+    [ObservableProperty]
+    [NotifyCanExecuteChangedFor(nameof(HandleAddClickCommand))]
+    private string? _username;
+
+    public MainViewModel()
     {
-        [ObservableProperty]
-        private string _headingColumn1 = "Add To Database";
+        SQLitePCL.Batteries.Init();
+        using var connection = new SqliteConnection("Data Source=" + DbPath);
+        connection.Open();
+        if (SqlUtils.TableAlreadyExists(connection, DbTableName))
+            return;
 
-        [ObservableProperty]
-        private string _headingColumn2 = "Read From Database";
+        var command = connection.CreateCommand();
+        command.CommandText =
+            @"
+            CREATE TABLE user (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL
+            );
 
-        [ObservableProperty]
-        private string _headingColumn3 = "Log";
+            INSERT INTO user
+            VALUES (1, 'Brice'),
+                   (2, 'Alexander'),
+                   (3, 'Nate');
+            ";
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
 
-        [ObservableProperty]
-        private string _textColumn2 = "Database Contents:";
+    private bool CanAddToDb() => !string.IsNullOrEmpty(Username);
 
-        [ObservableProperty]
-        private string _textColumn3 = "Log:";
+    [RelayCommand(CanExecute = nameof(CanAddToDb))]
+    private async Task HandleAddClick()
+    {
+        await Task.Delay(TimeSpan.FromSeconds(3));
+        AddToDb();
+    }
 
-        [RelayCommand]
-        private void HandleAddClick()
+    [RelayCommand]
+    private void HandleReadClick()
+    {
+        ReadFromDb();
+    }
+
+    private void AppendToTextColumn2(string text)
+    {
+        TextColumn2 += $"\n{text}";
+    }
+
+    private void AppendToTextColumn1(string text)
+    {
+        TextColumn1 += $"\n{text}";
+    }
+
+    private void AddToDb()
+    {
+        using var connection = new SqliteConnection($"Data Source={DbPath}");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText =
+            @"
+            INSERT INTO user (name)
+            VALUES ($name)
+            ";
+        command.Parameters.AddWithValue("$name", Username);
+        command.ExecuteNonQuery();
+        AppendToTextColumn2($"user with name {Username} inserted into {DbTableName}.");
+
+        command.CommandText =
+            @"
+            SELECT last_insert_rowid()
+            ";
+        _dbLastRowId = (long)(command.ExecuteScalar() ?? 0);
+        AppendToTextColumn2($"`_dbLastRowId` set to {_dbLastRowId}.");
+        connection.Close();
+    }
+
+    private void ReadFromDb()
+    {
+        using var connection = new SqliteConnection($"Data Source={DbPath}");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText =
+            @"
+            SELECT name
+            FROM user
+            WHERE id = $id
+            ";
+        command.Parameters.AddWithValue("$id", _dbLastRowId);
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
         {
-            TestSqlite();
+            var name = reader.GetString(0);
+
+            AppendToTextColumn1($"Hi, {name}!");
+            AppendToTextColumn2($"Read {name} from db.");
         }
-
-        private void AppendToTextColumn2(string text)
-        {
-            TextColumn2 += "\n" + text;
-        }
-
-        private void TestSqlite()
-        {
-            SQLitePCL.Batteries.Init();
-            long id;
-            using (var connection = new SqliteConnection("Data Source=hello.db"))
-            {
-                connection.Open();
-
-                var command = connection.CreateCommand();
-                command.CommandText =
-                    @"
-                    CREATE TABLE user (
-                        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                        name TEXT NOT NULL
-                    );
-
-                    INSERT INTO user
-                    VALUES (1, 'Brice'),
-                           (2, 'Alexander'),
-                           (3, 'Nate');
-                    ";
-                command.ExecuteNonQuery();
-
-                var name = "Tim";
-
-                command.CommandText =
-                    @"
-                    INSERT INTO user (name)
-                    VALUES ($name)
-                    ";
-                command.Parameters.AddWithValue("$name", name);
-                command.ExecuteNonQuery();
-
-                command.CommandText =
-                    @"
-                    SELECT last_insert_rowid()
-                    ";
-                id = (long)(command.ExecuteScalar() ?? 0);
-
-                AppendToTextColumn2($"Your new user ID is {id}.");
-            }
-
-            using (var connection = new SqliteConnection("Data Source=hello.db"))
-            {
-                connection.Open();
-
-                var command = connection.CreateCommand();
-                command.CommandText =
-                    @"
-                    SELECT name
-                    FROM user
-                    WHERE id = $id
-                    ";
-                command.Parameters.AddWithValue("$id", id);
-
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var name = reader.GetString(0);
-
-                        AppendToTextColumn2($"Hello, {name}!");
-                    }
-                }
-            }
-        }
+        connection.Close();
     }
 }
